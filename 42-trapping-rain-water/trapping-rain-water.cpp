20fb0288867aa7cd822da37e74eb09e11b389895
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-06-08 23:04:36 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (93.26%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        int l = 0, r = n - 1;
+        int lmax = 0, rmax = 0;
+        int total = 0;
+        
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] >= lmax) {
+                    lmax = height[l];
+                } else {
+                    total += lmax - height[l];
+                }
+                l++;
+            } else {
+                if (height[r] >= rmax) {
+                    rmax = height[r];
+                } else {
+                    total += rmax - height[r];
+                }
+                r--;
+            }
+        }
+        
+        return total;
+    }
+};
