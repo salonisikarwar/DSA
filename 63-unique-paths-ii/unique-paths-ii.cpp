fc7443adc27266b455819f9bd321a4a4f1803565
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-07-05 18:23:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (80.65%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -3,27 +3,33 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size();
         int m = obstacleGrid[0].size();
-        
-        vector<vector<int>> dp(n, vector<int>(m, 0));
+
+        const int mod = 1e9 + 7; // Optional: Only needed if modulus is required
+
+        vector<int> prev(m, 0);
 
         for(int i = 0; i < n; i++) {
+            vector<int> curr(m, 0);
+
             for(int j = 0; j < m; j++) {
-                // If there's an obstacle
                 if(obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
+                    curr[j] = 0;
                 }
-                // Starting cell
                 else if(i == 0 && j == 0) {
-                    dp[i][j] = 1;
+                    curr[j] = 1;
                 }
                 else {
                     int up = 0, left = 0;
-                    if(i > 0) up = dp[i-1][j];
-                    if(j > 0) left = dp[i][j-1];
-                    dp[i][j] = up + left;
+                    if(i > 0) up = prev[j];
+                    if(j > 0) left = curr[j-1];
+                    curr[j] = up + left; // or use: (up + left) % mod;
                 }
             }
+
+            prev = curr;
         }
-        return dp[n-1][m-1];
+
+        return prev[m-1];
     }
 };
+
