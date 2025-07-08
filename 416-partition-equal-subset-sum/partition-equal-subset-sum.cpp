397e34f989b19b91bd1a62349706a381c716851d
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-07-08 18:48:41 +0530

    Time: 131 ms (56.28%) | Memory: 13.4 MB (93.87%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
- bool isSubsetSum(vector<int>& arr, int sum) {
-        // code here
-             int n = arr.size();
+ bool subsetSumToK(int n, int k, vector<int>& arr) {
+        // Base DP arrays (only 2 rows to save space)
+        vector<bool> prev(k + 1, false), cur(k + 1, false);
 
-vector<vector<bool>> dp(n, vector<bool>(sum + 1, false));
-        for(int i =0 ;i<n;i++){dp[i][0] = true;
-        }
-         if (arr[0] <= sum)
-            dp[0][arr[0]] = true;
-        
-        for(int ind =1;ind<=n-1 ;ind++)
-        {
-            for(int target =1 ;target<= sum;target++)
-            {
-                bool nottake =dp[ind-1] [target ];
-            bool take = false;
-            if(target>= arr[ind])
-      {
-          take = dp[ind-1][ target - arr[ind]];
-          
-      }
-      dp[ind][target] = take || nottake;
-                
+        // Always possible to make sum 0 (empty subset)
+        prev[0] = cur[0] = true;
+
+        // If first element fits in sum range
+        if (arr[0] <= k)
+            prev[arr[0]] = true;
+
+        // Loop through the rest of the elements
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= k; target++) {
+                bool notTake = prev[target];
+                bool take = false;
+
+                if (arr[ind] <= target)
+                    take = prev[target - arr[ind]];
+
+                cur[target] = take || notTake;
             }
+            prev = cur; // Move to the next row
         }
-                return dp[n - 1][sum];
 
+        return prev[k];
     }
 
     bool canPartition(vector<int>& nums) {
          int totalsum = 0;
-         for(int i = 0 ;i<nums.size();i++)
+         int n =nums.size();
+         for(int i = 0 ;i<n;i++)
          {
             totalsum += nums[i];
          }
          if(totalsum%2)return false;
          int target  = totalsum/2;
-         return isSubsetSum(nums, target);
+         return subsetSumToK(n, target,nums);
     }
 };
