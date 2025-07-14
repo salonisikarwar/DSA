20ceb002f68c751c288d34a2a4b9b32050f4d359
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-07-14 23:12:39 +0530

    Time: 3 ms (74.38%) | Memory: 9.9 MB (93.41%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
+ int char2num(char a) {
+     switch (a) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+
     int romanToInt(string s) {
-        unordered_map<char, int> roman = {
-            {'I', 1}, {'V', 5}, {'X', 10},
-            {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-        };
-        
-        int total = 0;
-        int n = s.length();
-        
-        for(int i = 0; i < n; i++) {
-            int value = roman[s[i]];
-            
-            // If current symbol is less than next symbol, subtract it
-            if(i + 1 < n && value < roman[s[i + 1]]) {
-                total -= value;
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() && char2num(s[i]) < char2num(s[i + 1])) {
+                result -= char2num(s[i]);
             } else {
-                total += value;
+                result += char2num(s[i]);
             }
         }
-        
-        return total;
+        return result;
     }
         
     
