8696b973fdde50b8c79d2bc08fb985f2c3e7576f
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-07-24 22:36:21 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (33.30%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxInd = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (i > maxInd) return false; // Can't reach current index
+            maxInd = max(maxInd, i + nums[i]);
+            if (maxInd >= n - 1) return true; // Early exit: end is reachable
+        }
+        return true;
+    }
+};
