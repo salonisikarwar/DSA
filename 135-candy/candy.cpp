265ea7c5146aeaa294d8fc120d4d4755dadd8354
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-07-27 05:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (85.72%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,27 +2,35 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> left(n, 1);
+        if (n == 0) return 0;
 
-        // Left to right pass
-        for (int i = 1; i < n; i++) {
+        int sum = 1; // First child always gets 1 candy
+        int up = 0, down = 0, peak = 0;
+
+        for (int i = 1; i < n; ++i) {
             if (ratings[i] > ratings[i - 1]) {
-                left[i] = left[i - 1] + 1;
+                // Increasing slope
+                up++;
+                peak = up;
+                down = 0;
+                sum += up + 1;  // +1 for the current child
             }
-        }
+            else if (ratings[i] < ratings[i - 1]) {
+                // Decreasing slope
+                up = 0;
+                down++;
+                sum += down;
 
-        int sum = left[n - 1]; // Start from last child
-        int curr = 1;
-
-        // Right to left pass
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                curr += 1;
-            } else {
-                curr = 1;
+                if (down > peak) {
+                    // Add one extra to compensate peak child if needed
+                    sum += 1;
+                }
             }
-
-            sum += max(left[i], curr);
+            else {
+                // Equal ratings
+                up = down = peak = 0;
+                sum += 1;
+            }
         }
 
         return sum;
