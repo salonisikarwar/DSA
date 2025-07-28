fb4b7f268e5588d9a313630c9a52748b0e7484af
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-07-29 00:15:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (52.35%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
-    int xorOperation(int n, int start) {
-        int result = 0;
-    for (int i = 0; i < n; ++i) {
-        result ^= (start + 2 * i);
-    }
-    return result;
-        
-    }
+   int xorFrom0To(int x) {
+    if (x % 4 == 0) return x;
+    if (x % 4 == 1) return 1;
+    if (x % 4 == 2) return x + 1;
+    return 0;
+}
+
+int xorOperation(int n, int start) {
+    int x = start / 2;
+    int r = start % 2;
+    int xorVal = xorFrom0To(x + n - 1) ^ xorFrom0To(x - 1);
+    return xorVal * 2 + (n % 2) * r;
+}
 };
