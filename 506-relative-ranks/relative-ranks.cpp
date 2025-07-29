a5b85edf9281acdc9a57f658534b442dabab40d0
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-07-30 00:01:20 +0530

    Time: 5 ms (54.61%) | Memory: 16.2 MB (50.72%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-         int n = score.size();
-        vector<int> sorted = score;
-        sort(sorted.begin(), sorted.end(), greater<int>());
-
-        unordered_map<int, string> rank;
+          int n = score.size();
+        int maxScore = *max_element(score.begin(), score.end());
+        
+        // Bucket for counting sort
+        vector<int> index(maxScore + 1, -1);
+        
         for (int i = 0; i < n; ++i) {
-            if (i == 0) rank[sorted[i]] = "Gold Medal";
-            else if (i == 1) rank[sorted[i]] = "Silver Medal";
-            else if (i == 2) rank[sorted[i]] = "Bronze Medal";
-            else rank[sorted[i]] = to_string(i + 1);
+            index[score[i]] = i;
         }
 
-        vector<string> result;
-        for (int i = 0; i < n; ++i) {
-            result.push_back(rank[score[i]]);
+        vector<string> res(n);
+        int rank = 1;
+
+        for (int s = maxScore; s >= 0; --s) {
+            if (index[s] != -1) {
+                string r;
+                if (rank == 1) r = "Gold Medal";
+                else if (rank == 2) r = "Silver Medal";
+                else if (rank == 3) r = "Bronze Medal";
+                else r = to_string(rank);
+                res[index[s]] = r;
+                ++rank;
+            }
         }
 
-        return result;
-    
+        return res;
         
     }
 };
