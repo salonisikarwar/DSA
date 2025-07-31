c4755b1e9c487f06436403e64f0351a04c1dc2f2
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-07-31 23:23:52 +0530

    Time: 8 ms (26.58%) | Memory: 18.2 MB (91.31%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,53 +1,60 @@
+class compare {
+public:
+    bool operator()(ListNode* a, ListNode* b) {
+        return a->val > b->val; // min-heap
+    }
+};
 class Solution {
 public:
-    // Max-heapify
-    void heapify(vector<int>& arr, int n, int i) {
-        int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
+   
 
-        if (left < n && arr[left] > arr[largest])
-            largest = left;
-        if (right < n && arr[right] > arr[largest])
-            largest = right;
-
-        if (largest != i) {
-            swap(arr[i], arr[largest]);
-            heapify(arr, n, largest);
-        }
-    }
 
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int> values;
+ priority_queue<ListNode*, vector<ListNode*>, compare> minHeap;
 
-        // Step 1: Extract all node values into the vector
-        for (auto list : lists) {
-            while (list != nullptr) {
-                values.push_back(list->val);
-                list = list->next;
-            }
+
+       int  k  = lists.size();
+
+       //step 1:
+       for(int i= 0;i<k;i++)
+       {
+        if(lists[i] != NULL )
+        {
+            minHeap.push(lists[i]);
         }
+       }
+       ListNode* head = NULL;
+       ListNode*tail = NULL;
+       
+       while(!minHeap.empty())
+       {
+        ListNode* top  = minHeap.top();
+        minHeap.pop();
 
-        // Step 2: Build max heap
-        int n = values.size();
-        for (int i = n / 2 - 1; i >= 0; i--) {
-            heapify(values, n, i);
+        if(top->next != NULL)
+        {
+            minHeap.push(top->next);
         }
-
-        // Step 3: Heap sort to get sorted values in ascending order
-        for (int i = n - 1; i > 0; i--) {
-            swap(values[0], values[i]);
-            heapify(values, i, 0); // reduce size
+        if(head == NULL)
+        {
+            head   = top;
+            tail  =top  ;
         }
+            /*if(tail->next != NULL)
+            {
+                minHeap.push(tail->next);
+            }*/
+            else{
+                tail->next = top ;
+                tail  =top ;}
 
-        // Step 4: Create new linked list from sorted values
-        ListNode* dummy = new ListNode(0);
-        ListNode* tail = dummy;
-        for (int val : values) {
-            tail->next = new ListNode(val);
-            tail = tail->next;
+                /*if(tail->next != NULL)
+                {
+                    minHeap.push(tail->next);
+                }
+            }*/
         }
-
-        return dummy->next;
-    }
+        return head;
+       }
+    
 };
