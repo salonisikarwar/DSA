da4b465d5df86c26d37b5494649540690ed8bbb3
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-08-04 19:04:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (26.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-    sort(begin(s) , end(s));
-    sort(begin(t) , end(t));
-    return (s ==t) ;
-        
+    vector<int> count(26 , 0);
+
+    for(char &ch :s)
+    {
+        count[ch-'a']++;
+    }
+    for(char &ch :t)
+    {
+        count[ch-'a']--;
+    }
+
+    // check if all elements in count are zero 
+    //for loop 
+    //or
+    //lamda like below
+    bool allZeros  = all_of(begin(count) , end(count) , [](int element){
+     return element ==0;
+});
+return allZeros;
     }
 };
