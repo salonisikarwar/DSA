885404ceee733863bab3b582b7356f4f994dc2d5
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-08-04 16:06:46 +0530

    Time: 7 ms (51.43%) | Memory: 12.3 MB (61.93%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
- int char2num(char a) {
-     switch (a) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default: return 0;
-        }
-    }
+
+       
 
     int romanToInt(string s) {
+         unordered_map<char , int>mp;
+            mp[ 'I'] = 1;
+            mp[ 'V']= 5;
+            mp[ 'X'] = 10;
+            mp[ 'L'] = 50;
+            mp ['C' ]= 100;
+            mp ['D' ]= 500;
+            mp ['M' ]= 1000;
+        
+    
         int result = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (i + 1 < s.length() && char2num(s[i]) < char2num(s[i + 1])) {
-                result -= char2num(s[i]);
+            if (mp[s[i]] <mp[s[i+1]]) {
+                result -= mp[s[i]];
             } else {
-                result += char2num(s[i]);
+                result += mp[s[i]];
             }
         }
         return result;
