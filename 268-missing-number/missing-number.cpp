c76641a7beaa041fe8e02d0f9c1748834edf893b
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-09-05 15:22:00 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (38.09%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int XOR1 = 0;
-        int n = nums.size();
-        for(int i= 0;i<=n;i++)
-        {
-            XOR1 = XOR1^i;
-
-        }
-       int  XOR2 = 0;
-        for(int i=0; i<n ;i++)
-        {
-            XOR2 = XOR2^nums[i];
-        }
-        return XOR1^XOR2;
-        
+        int n  = nums.size();
+     int totalsum = n*(n+1)/2;
+     int actual_sum =0;
+     for(int i  = 0 ;i<n ;i++)
+     {
+        actual_sum += nums[i];
+     }
+     return totalsum- actual_sum;
     }
 };
