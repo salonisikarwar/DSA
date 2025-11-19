4c20e39b3a95d684249d0665e29dbe15b5f491f9
salonisikarwar <salonisinghsikarwar8@gmail.com>
2025-11-19 10:33:03 +0530

    Time: 4 ms (46.71%) | Memory: 11.3 MB (65.23%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,42 +1,52 @@
+								
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        int n = num.size();
-        stack<char> st;
+        if(num.length() <= k)   
+            return "0";
         
-        for(int i = 0; i < n; i++) {
-            // While we can remove more digits and the top of stack is greater than current digit
-            while(!st.empty() && k > 0 && st.top() > num[i]) {
-                st.pop();
-                k--;
-            }
-            st.push(num[i]);
-        }
+        if(k == 0)
+            return num;
         
-        // If k > 0, remove from the end
-        while(k > 0 && !st.empty()) {
-            st.pop();
-            k--;
-        }
-        
-        // Build the result from the stack (will be reversed order)
         string res = "";
-        while(!st.empty()) {
-            res += st.top();
-            st.pop();
+        stack <char> s;
+        
+        s.push(num[0]); 
+    
+        for(int i = 1; i<num.length(); ++i)
+        {
+            while(k > 0 && !s.empty() && num[i] < s.top())
+            {
+               
+                --k;
+                s.pop();
+            }
+            
+            s.push(num[i]);
+            
+            if(s.size() == 1 && num[i] == '0')
+                s.pop();
         }
         
-        // Reverse the string since stack gives digits in reverse
-        reverse(res.begin(), res.end());
-        
-        // Remove leading zeros
-        while(res.size() > 1 && res[0] == '0') {
-            res.erase(res.begin());
+        while(k && !s.empty())
+        {
+            --k;
+            s.pop();
         }
         
-        // If empty after removing zeros, return "0"
-        if(res.empty()) return "0";
+        while(!s.empty())
+        {
+            res.push_back(s.top()); 
+            s.pop();
+        }
+        
+        reverse(res.begin(),res.end()); 
+        
+        if(res.length() == 0)
+            return "0";
         
         return res;
+        
+        
     }
 };
