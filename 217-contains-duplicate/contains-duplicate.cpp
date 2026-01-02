34311b24db9bdc44dc7fbbfb2cbf5a9dffb49b75
salonisikarwar <salonisinghsikarwar8@gmail.com>
2026-01-02 12:00:08 +0530

    Time: 108 ms (6.90%) | Memory: 92.4 MB (20.81%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int, int> freq;
+
+        for (int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+            if (freq[nums[i]] > 1)
+                return true;
+        }
+        return false;
+    }
+};
