8dfdcbdfa288b59937332421d599549e7e577562
salonisikarwar <salonisinghsikarwar8@gmail.com>
2026-01-10 23:46:02 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (58.16%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,20 +1,23 @@
 class Solution {
- public:
-  vector<int> productExceptSelf(vector<int>& nums) {
-    const int n = nums.size();
-    vector<int> ans(n);        // Can also use `nums` as the ans array.
-    vector<int> prefix(n, 1);  // prefix product
-    vector<int> suffix(n, 1);  // suffix product
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> answer(n, 1);
 
-    for (int i = 1; i < n; ++i)
-      prefix[i] = prefix[i - 1] * nums[i - 1];
+        // Step 1: Build prefix product into 'answer'
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
 
-    for (int i = n - 2; i >= 0; --i)
-      suffix[i] = suffix[i + 1] * nums[i + 1];
+        // Step 2: Multiply by suffix product on the fly
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
 
-    for (int i = 0; i < n; ++i)
-      ans[i] = prefix[i] * suffix[i];
-
-    return ans;
-  }
+        return answer;
+    }
 };
