a76cd8d92180b9d3048ea76f19c86431ae0a2ac4
salonisikarwar <salonisinghsikarwar8@gmail.com>
2026-01-11 22:12:49 +0530

    Time: 11 ms (14.43%) | Memory: 24.8 MB (13.48%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>>mergedIntervals;
-        if(intervals.size()==0)
-        {
-            return mergedIntervals;
-        }
-        sort(intervals.begin(),  intervals.end());
-        vector<int> tempInterval  = intervals[0];
+        if(intervals.size()==0)return mergedIntervals;
+        sort(intervals.begin(), intervals.end());
+        vector<int>tempInterval= intervals[0];
         for(auto it:intervals){
-            if(it[0] <= tempInterval[1])
+        if(tempInterval[1]>=it[0])
+        {
+            tempInterval[1] = max(it[1] , tempInterval[1]);
 
-            {
-                tempInterval[1] = max(it[1] , tempInterval[1]);
-
-            }
-            else{
-                mergedIntervals.push_back(tempInterval);
-                tempInterval  = it;
-            }
         }
-        mergedIntervals.push_back(tempInterval);
+        else{
+            mergedIntervals.push_back(tempInterval);
+                tempInterval  = it;
+        }
+
+
+        }
+                            mergedIntervals.push_back(tempInterval);
+
         return mergedIntervals;
+        
     }
 };
