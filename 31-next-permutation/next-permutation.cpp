e4d08829e3d1d11ce156cb27e5703de73432c2c0
salonisikarwar <salonisinghsikarwar8@gmail.com>
2026-06-02 22:19:31 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (31.99%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
         int index = -1;
-
-        // Step 1: Find the first decreasing element from the right
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
+        int n = nums.size();
+        for(int i =  n-2 ;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
                 index = i;
                 break;
             }
         }
-
-        // Step 2: If no such index, array is the last permutation → reverse it
-        if (index == -1) {
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-
-        // Step 3: Find element just larger than nums[index]
-        for (int i = n - 1; i > index; i--) {
-            if (nums[i] > nums[index]) {
-                swap(nums[i], nums[index]);
+        if(index==-1){
+        reverse(nums.begin(),nums.end());
+        return; }
+        for(int i =n-1;i>index;i--)
+        {
+            if(nums[i]>nums[index])
+            {
+                swap(nums[index], nums[i]);
                 break;
             }
         }
-
-        // Step 4: Reverse the suffix
-        reverse(nums.begin() + index + 1, nums.end());
+        reverse(nums.begin()+index+1 , nums.end());
     }
 };
